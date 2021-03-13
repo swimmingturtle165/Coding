54d1863a308f158a58784d8a87197c40b75f3c8d
sachitjaggi <sachitjaggi.1562@gmail.com>
2021-03-13 11:32:51 +0530

    13.03.2021

---
+++ ITMO Course/Segment Tree/Segment Tree Part 1/Step 1/implementation_min_cnt.cpp
@@ -0,0 +1,236 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+typedef     unsigned long long int ull;
+typedef     long long int    ll;
+typedef     long double      ld;
+typedef     pair<ll,ll>      pll;
+#define     FOR(i,a,b)       for(ll i=a;i<b;i++)
+#define     FORE(i,a,b)      for(int i=a;i<=b;i++)
+#define     FORD(i,b,a)      for(int i=b;i>a;i--)
+#define     FORDE(i,b,a)     for(ll i=b;i>=a;i--)
+#define     debug(x)         cout<< '>'<<#x<<" : "<<x<<"\n";
+#define     debug2(x,y)      cout<< '>'<<#x<<" : "<<x<<"\n"; cout<< '>'<<#y<<" : "<<y<<"\n";
+#define     debug3(x,y,z)    cout<< '>'<<#x<<" : "<<x<<"\n"; cout<< '>'<<#y<<" : "<<y<<"\n";cout<< '>'<<#z<<" : "<<z<<"\n";
+#define     umap             unordered_map
+#define     uset             unordered_set
+#define     lb               lower_bound
+#define     ub               upper_bound
+#define     mp               make_pair
+#define     in               insert
+#define     s                second
+#define     f                first
+#define     nn               cout<<"\n"
+#define     pb               push_back
+#define     testcase         int t;cin>>t;while(t--)
+#define     gcd(a,b)         __gcd(a,b)
+#define     maxv             INT_MAX
+#define     minv             INT_MIN
+#define     MOD              1000000007
+#define     FastIO           ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
+#define     here             cout<<"I'm here\n";
+#define     flush            fflush(stdout);
+#define endl '\n'         
+
+   
+struct segmenttree{
+
+    ll sz=1;
+
+    vector<pll> segtree;
+
+    void init(ll n)
+    {
+        sz = 1;
+        while (sz<n)
+        {
+            sz = sz * 2;
+        }
+        segtree.assign(2 * sz, {INT_MAX,0});
+    }
+
+    void build(vector<ll>&a, ll x, ll lx ,ll rx)
+    {
+
+        if(rx-lx==1)
+        {
+            // leaf node
+            if(lx<a.size())
+            {
+                segtree[x].first = a[lx];
+                segtree[x].second =1;
+
+            }
+            return;
+        }
+        ll mid = (lx + rx) / 2;
+        build(a, 2 * x + 1, lx, mid);
+        build(a, 2 * x + 2, mid,rx);
+        if(segtree[2 * x + 1].first < segtree[2 * x + 2].first)
+        {
+            segtree[x] = segtree[2 * x + 1];
+
+        }
+        else if(segtree[2 * x + 1].first > segtree[2 * x + 2].first)
+        {
+        segtree[x] =  segtree[2 * x + 2];
+
+        }
+        else
+        {
+            segtree[x] = segtree[2 * x + 1] ;
+            segtree[x].second += segtree[2 * x + 2].second;
+
+        }
+
+
+    }
+
+    void build(vector<ll>&a)
+    {
+        build(a, 0, 0, sz);
+    }
+
+    void set(ll idx, ll val,ll x,ll lx, ll rx)
+    {
+        if(rx-lx==1)
+        {
+            segtree[x].first = val;
+            segtree[x].second =1;
+            return;
+        }
+        ll mid = (lx + rx) / 2;
+        
+        if(idx<mid)
+        {
+            // left child
+            set(idx, val, 2 * x + 1, lx, mid);
+        }
+        else
+        {
+            // right child 
+            set(idx, val, 2 * x + 2, mid,rx);
+
+        }
+         if(segtree[2 * x + 1].first < segtree[2 * x + 2].first)
+        {
+            segtree[x] = segtree[2 * x + 1];
+
+        }
+        else if(segtree[2 * x + 1].first > segtree[2 * x + 2].first)
+        {
+        segtree[x] =  segtree[2 * x + 2];
+
+        }
+        else
+        {
+            segtree[x] = segtree[2 * x + 1] ;
+            segtree[x].second += segtree[2 * x + 2].second;
+
+        }
+
+    }
+
+    void set(ll idx, ll val)
+    {
+        set(idx, val, 0, 0, sz);
+    }
+
+    pll get_segment(ll l, ll r,ll x,ll lx ,ll rx)
+    {
+
+        //  get sum from l to r-1
+        // we currently store in x sum from lx to rx-1
+        
+
+        if(l>=rx || r<=lx)
+        {
+            // don't intersect
+            return {INT_MAX,0};
+        }
+
+        if(l<=lx && rx<=r)
+        {
+            //  complete intersection
+             return segtree[x]; 
+        }
+        
+        ll mid = (lx + rx) / 2;
+        pll answ;
+
+        pll left = get_segment(l, r, 2 * x + 1, lx, mid);
+        pll right=get_segment(l, r, 2 * x + 2, mid,rx);
+        
+
+        if(left.first < right.first)
+        {
+            return left;
+        }
+        else if(left.first > right.first)
+        {
+            return right;
+        }
+        else
+        {
+            answ = left;
+            answ.second += right.second;
+            return answ;
+        }
+        
+    
+
+        
+    }
+    
+    pll get_segment(ll l, ll r)
+    {
+        return get_segment(l, r, 0, 0, sz);
+    }
+};
+
+signed main(int argc, char** argv)
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    FastIO;
+    long t=1;
+    // cin>>t;
+    while(t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        vector<ll> inp(n);
+        segmenttree arr;
+        arr.init(n);
+        FOR(i, 0, n)
+        {
+         cin >> inp[i];
+        }
+        arr.build(inp);
+        while (m--)
+        {
+            ll op;
+            cin >> op;
+            if(op==1)
+            {
+                ll idx, val;
+                cin >> idx >> val;
+                arr.set(idx, val);
+            }
+            else
+            {
+                ll left, right;
+                cin >> left >> right;
+                pll answ = arr.get_segment(left, right);
+                cout << answ.first << " " << answ.second<< endl;
+            }
+        }
+    }
+    return 0;
+}
