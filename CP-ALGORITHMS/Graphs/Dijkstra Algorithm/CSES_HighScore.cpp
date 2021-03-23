938318244fb1a43cf02e67f8d26b543a80e3ab4c
sachitjaggi <sachitjaggi.1562@gmail.com>
2021-03-23 12:09:54 +0530

    23.03,2021

--- CP-ALGORITHMS/Graphs/Dijkstra Algorithm/CSES_HighScore.cpp
+++ CP-ALGORITHMS/Graphs/Dijkstra Algorithm/CSES_HighScore.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp> 
 #include <ext/pb_ds/tree_policy.hpp> 
-
+ 
 using namespace std;
 using namespace __gnu_pbds; 
-
+ 
 typedef     unsigned long long int ull;
 typedef      long long     ll;
 typedef     long double      ld;
@@ -36,11 +36,23 @@ typedef     pair< ll, ll>      pll;
 #define     flush            fflush(stdout);
 #define endl '\n'         
 #define ordered_set_single tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+ 
+ 
+void dfs(int curr,vector<bool>& conn, vector<vector<pll>>& graph1)
+{
 
+    if(conn[curr])
+    {
+        return;
+    }
+    conn[curr]=true;
+    for(auto it:graph1[curr])
+    {
+        dfs(it.first,conn,graph1);
+    }
 
-   
-
-
+}
+ 
 signed main(int argc, char** argv)
 {
     // #ifndef ONLINE_JUDGE
@@ -54,45 +66,74 @@ signed main(int argc, char** argv)
     {
         ll n,m;
         cin>>n>>m;
-
+ 
         vector<vector<pll>> graph(n+1,vector<pll>());
+        vector<vector<pll>> graph1(n+1,vector<pll>());
+
         vector<ll> dp(n+1,INT64_MIN);
         vector<ll> maxi(n+1,0);
         vector<bool> visited(n+1,false);
 
-        dp[0]=dp[1]=0;
+        vector<bool> conn(n+1,false);
+
+ 
+        
+
         FOR(i,0,m)
         {
             ll a,b,c;
             cin>>a>>b>>c;
             graph[a].pb({b,c});
-        }
+            graph1[b].pb({a,c});
 
+        }
+        dfs(n,conn,graph1);
+        // FOR(i,0,n+1)
+        // {
+        //     cout<<i<<" "<<conn[i]<<endl;
+        // }
         // check for +ve weight cycle
         priority_queue<pll> pq;
-
+ 
         pq.push({0,1});
         ll ans=0;
         while(pq.size()>0)
         {
             ans++;
-            if(ans>=n*n)
-            {
-                cout<<-1<<endl;
-                return 0;
-            }
+            
             pll curr=pq.top();
             pq.pop();
             ll idx=curr.s;
             ll cst=curr.f;
             // cout<<idx<<" "<<cst<<endl;
-            if(visited[idx])
+             if(visited[idx] &&   dp[idx]<cst)
             {
+                // cout<<idx<<" "<<dp[idx]<<" "<<cst<<endl;
+
+                // cout<<"**"<<endl;
+                // checks for a positive cycle
+
+                if(conn[idx])
+                {
+                cout<<-1<<endl;
+                return 0;
+                }
+                else
+                {
+                    continue;
+                }
+                
+                
+            }
+            else if(visited[idx] && dp[idx]>=cst)
+            {
+                // cout<<idx<<" "<<dp[idx]<<" "<<cst<<endl;
                 continue;
             }
+            
+            dp[idx]=cst;
             visited[idx]=true;
-            ans=max(ans,cst);
-
+ 
             for(auto v:graph[idx])
             {
                 ll dst=v.f;
@@ -102,22 +143,22 @@ signed main(int argc, char** argv)
                 {
                     // cout<<"**"<<endl;
                     ll k1=price+dp[idx];
-                    dp[dst]=k1;
-                    pq.push({dp[dst],dst});
+                    // dp[dst]=k1;
+                    pq.push({k1,dst});
                 }
                 // cout<<dst<<" "<<price<<" "<<idx<<" "<<dp[idx]<<" "<<dp[dst]<<" "<<maxi[dst]<<endl;
-
+ 
             }
-
+ 
         }
         // dispvector<ll>(dp);
         // dispvector<ll>(maxi);
-
+ 
         // cout<<maxi[n]<<" "<<dp[n]<<endl;
         // cout<<dp[n]<<endl;
         cout<<dp[n]<<endl;
         // FOR(i,0,n) cout<<dp[i+1]<<" ";
-
+ 
         // cout<<endl;
     }
     return 0;
